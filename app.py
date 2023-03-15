9bd70571c4d177332988e5f01e15bbb993313d68
A-Rifkan <a.rifkan2019@gmail.com>
2023-03-15 14:38:58 +0000

    Weather app done

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 apiKey = 'efabeec0700c68be97e8a97c342ea5e1'
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
@@ -14,20 +14,20 @@ def weather():
         city = request.form['city']
         print(city)
     # Replace YOUR_API_KEY with the actual API key obtained from OpenWeatherMap
-    url = f'https://api.openweathermap.org/data/2.5/weather?{city}&appid={apiKey}&units=metric'
-    
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric'
+
     # Fetch the data from the API
     response = requests.get(url)
     data = response.json()
 
     # Extract relevant data from the API response
     description = data['weather'][0]['description']
-    temperature = data['main']['temp']
+    temp = data['main']['temp']
     humidity = data['main']['humidity']
 
     # Return the weather data in the response
     # return f'{city} weather: {description}, Temperature: {temperature} C, Humidity: {humidity}%'
-    return render_template('weather.html')
-    
+    return render_template('index.html', temp = temp, description = description, humidity = humidity)
+
 if __name__ == '__main__':
     app.run()
